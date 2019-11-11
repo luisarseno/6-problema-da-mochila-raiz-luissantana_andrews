666ccd23642da5cfb1e0c6f3f8a96df0271b67d9
Andrews Souza <andrews.souza@cwi.com.br>
2019-11-11 20:49:08 -0300

    corrigindo max-tab

--- index-max-tab.js
+++ index-max-tab.js
@@ -2,16 +2,22 @@ class KnapSack {
   constructor(capacity, items) {
     this.capacity = capacity
     this.items = items
-    this.maxTab = this.initializeMaxTab(items)
+    this.maxTab = this.initializeMaxTab()
+
+    this.items.unshift(0)
   }
 
-  initializeMaxTab(items) {
-    const maxTab = new Array(items.length)
-    for (let i = 0; i < items.length; i++) {
+  initializeMaxTab() {
+    const maxTab = new Array(this.items.length)
+    for (let i = 0; i <= this.items.length; i++) {
 
-      maxTab[i] = new Array(this.capacity)
-      for (let j = 0; j < maxTab[i].length; j++) {
-        maxTab[i][j] = 0
+      maxTab[i] = new Array(this.capacity + 1)
+      for (let j = 0; j < this.capacity + 1; j++) {
+        if (j === 0) {
+          maxTab[i][j] = null
+        } else {
+          maxTab[i][j] = 0
+        }
       }
     }
 
@@ -19,22 +25,25 @@ class KnapSack {
   }
 
   calc() {
-    for (let i = 1; i < this.items.length; i++) {
-      for (let j = 1; j < this.capacity; j++) {
+    for (let i = 1; i < this.maxTab.length; i++) {
+      for (let j = 1; j < this.maxTab[i].length; j++) {
+        if (!!this.items[i] && this.items[i].weight <= j) {
+          const val1 = this.maxTab[i - 1][j]
+          const val2 = this.items[i].profit + this.maxTab[i - 1][j - this.items[i].weight]
 
-        if (this.items[i].weight <= j) {
-          this.maxTab[i][j] = this.max(this.maxTab[i - 1][j], this.items[i].profit + this.maxTab[i - 1][j - this.items[i].weight])
+          this.maxTab[i][j] = this.max(val1, val2)
         } else {
-          this.maxTab[i][j] = this.maxTab[i - 1][j] 
+          this.maxTab[i][j] = this.maxTab[i - 1][j]
         }
       }
     }
 
-    return this.maxTab[this.items.length - 1][this.capacity - 1]
+    return this.maxTab[this.items.length - 1][this.capacity]
   }
 
   max(val1, val2) {
-    return val1 >= val2 ? val1 : val2
+    const max = val1 >= val2 ? val1 : val2
+    return max
   }
 }
 
@@ -45,13 +54,15 @@ class Product {
   }
 }
 
-const knapSack = new KnapSack(7, [
-  new Product(5, 2),
-  new Product(2, 4),
-  new Product(1, 3),
-  new Product(2, 2),
+const knapSack = new KnapSack(15, [
+  new Product(5, 10),
+  new Product(5, 6),
+  new Product(3, 4),
+  new Product(1, 5),
+  new Product(2, 10),
 ])
 
 const answer = knapSack.calc()
+console.log(knapSack.maxTab)
 
-console.log(answer)
+console.log('resposta: ', answer)
